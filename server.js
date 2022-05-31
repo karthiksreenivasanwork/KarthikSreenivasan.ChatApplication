3600cef4bd44f1aabe88fb2fde024f4e46253cf9
Karthik Sreenivasan <101344162+karthiksreenivasanwork@users.noreply.github.com>
2022-05-31 13:01:20 +0530

    Completed the following --------------------------
    Added the following dependencies
    -------------------------------------
    1. socket.io NPM: Socket.IO is a library that enables low-latency, bidirectional and event-based communication between a client and a server. So when a client sends a message, the others are automatically notified of the new message without refreshing the page.
    2. body-parser NPM: Attach the body-parser npm as a middleware to receive and post the data as JSON.
    To receive use -> app.use(bodyParser.json());
    To post use -> app.use(bodyParser.urlencoded({ extended: false }));
    
    server.js:
    1. Get endpoint - messages: A HTTP get method that sents all the messages stored in the message collection.
    2. Post endpoint - message:  A HTTP post method that handles a new message sent by a client by adding it to a message collection. Additionally, it informs all the clients using socket.io to cascade the new message.
    3. io.on: The event that is fired when a new client has joined the chat.
    4. Use Node HTTP Server instead of express to serve the traffic that supports the chat and the socket requirement as socket.io can't work with express.js directly.
    
    index.html:
    1. Altered the JQuery library to use https://code.jquery.com/jquery-3.2.1.min.js instead of the slim version.
    2. User interface change to send and receive messages.
    3. An event handler using socket.io to update any messages posted by any clients using the code "socket.on("message", addMessage)" where the event name is called "message". More details added as comments in the application.

--- server.js
+++ server.js
@@ -1,7 +1,57 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 let app = express();
+/**
+ * Support for socket.io as it can't work with express directly.
+ */
+let http = require("http").Server(app); //Node HTTP Server.
+let io = require("socket.io")(http); //Initialize socket.io with the Node HTTP Server.
 
+/**
+ * The express.static() function is a built-in middleware function in Express.
+ * It serves static files and is based on serve-static
+ */
 app.use(express.static(__dirname));
-let server = app.listen(3000, () => {
+/**
+ * Attach the body-parser npm as a middleware to receive the body data.
+ * Here, we are letting body parser know that our expected data format is JSON
+ * for our HTTP requests.
+ */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let messages = [];
+
+/**
+ * A HTTP get method that sents all the messages stored in the message collection.
+ */
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+/**
+ * An HTTP post method that handles a new message sent by a client by adding it to a message collection.
+ * Additionally, it informs all the clients using socket.io to cascade the new message.
+ */
+app.post("/message", (req, res) => {
+  messages.push(req.body);
+  /**
+   * Update the chat message to all the client.
+   */
+  io.emit("message", req.body);
+  res.sendStatus(200);
+});
+
+/**
+ * The event that is fired when a new client has joined the chat.
+ */
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
+
+/**
+ * Start the server
+ */
+let server = http.listen(3000, () => {
   console.log(`Server is listening on the port : ${server.address().port}`);
 });
